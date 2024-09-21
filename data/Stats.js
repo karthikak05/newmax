67c08125ae3f3eb7a7fa69a7e2d115f599ef3faa
ArunKarthik05 <akkarthikk2002@gmail.com>
2024-09-21 19:37:21 +0530

    stats

--- data/Stats.js
+++ data/Stats.js
@@ -2,16 +2,16 @@ export const stats=[
     {
         heading: "Empower Your Team",
         para : "Equip your team with innovative tools that enhance productivity, foster collaboration, and drive results across all levels of your organization.",
-        image: "/icons/team.jpeg"
+        image: "/icons/team.png"
     },
     {
         heading: "Enhance Operational Efficiency",
         para: "Equip your team with innovative tools that enhance productivity, foster collaboration, and drive results across all levels of your organization.",
-        image: "/icons/efficiency.jpeg"
+        image: "/icons/efficiency.png"
     },
     {
         heading: "Drive Business Success",
         para : "Equip your team with innovative tools that enhance productivity, foster collaboration, and drive results across all levels of your organization.",
-        image: "/icons/success.jpeg"
+        image: "/icons/success.png"
     },
 ]
