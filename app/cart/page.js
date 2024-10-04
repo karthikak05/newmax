dfd5656aa865ebae0f6b2d3205633efcccbe15e9
ArunKarthik05 <akkarthikk2002@gmail.com>
2024-10-04 21:17:28 +0530

    localStorage

---
+++ app/cart/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./cartpage.module.scss";
+
+const Cart = () => {
+    return (
+        <main>
+            <h1>Your Cart, Your Spare Parts!</h1>
+            <p>Review the items in your cart before proceeding to checkout. Make sure you've got everything you need to keep your devices running smoothly.</p>
+        </main>
+    );
+};
+
+export default Cart;
