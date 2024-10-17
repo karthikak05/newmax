e5ae844047a850ec57ce3da4efdd10a77a74f60a
ArunKarthik05 <akkarthikk2002@gmail.com>
2024-10-17 09:43:34 +0530

    images:fetch

--- app/products/page.js
+++ app/products/page.js
@@ -3,12 +3,6 @@ import React from 'react';
 import styles from "./products.module.scss";
 import Product from '../../components/products/Product';
 
-export const metadata = {
-    title: "Products",
-    description: "All Mobile and Computer accessories in one place",
-  };
-  
-
 const Products = () => {
     return (
         <main className={styles.page}>
