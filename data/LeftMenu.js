a1f3bdd04546bff762ca8a20b43385e6ca029d81
ArunKarthik05 <akkarthikk2002@gmail.com>
2024-11-04 21:40:18 +0530

    zebra images

--- data/LeftMenu.js
+++ data/LeftMenu.js
@@ -8,6 +8,11 @@ export const LeftMenu = [
         { value: "Memor X3", label: "Memor X3", company: "Datalogic" },
         { value: "Skorpio X3", label: "Skorpio X3", company: "Datalogic" },
         { value: "Skorpio X4", label: "Skorpio X4", company: "Datalogic" },
+        { value: "MK1100 1200", label: "MK1100 1200", company: "Zebra" },
+        { value: "MK2250 MK2000 MK2046", label: "MK2250 MK2000 MK2046", company: "Zebra" },
+        { value: "MK3100 MK3190 MK3900", label: "MK3100 MK3190 MK3900", company: "Zebra" },
+        { value: "MK4000 MK4900", label: "MK4000 MK4900", company: "Zebra" },
+        { value: "MK500 MK590", label: "MK500 MK590", company: "Zebra" },
       ],      
     },
     {
