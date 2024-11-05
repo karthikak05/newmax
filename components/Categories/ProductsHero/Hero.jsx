b6515b071a0044ff325a1daa46b139b553b2b6ee
ArunKarthik05 <akkarthikk2002@gmail.com>
2024-11-06 01:00:18 +0530

    fix:categories hero

--- components/Categories/ProductsHero/Hero.jsx
+++ components/Categories/ProductsHero/Hero.jsx
@@ -5,15 +5,15 @@ import HeroImage from '@/components/Reusables/HeroImage/Hero-Image';
 export default function ProductsHero() {
   return (
     <div className={styles.main}>
-        <div className={styles.circle1}></div>
-        <div className={styles.circle2}></div>
-        <div className={styles.circle3}></div>
         <HeroImage 
             image="products-hero.png" 
             heading={<h1 className={styles.heading}>Quality Spare Parts for Your <span className={styles.textRed}>Mobile and Computer</span></h1>}
             para="Find the perfect spare parts for your mobile and computer devices. Our wide range includes screens, batteries, chargers, and more."
             bg="/bg-categories.png"  
         />
+        <div className={styles.circle1}></div>
+        <div className={styles.circle2}></div>
+        <div className={styles.circle3}></div>
     </div>
   )
 }
